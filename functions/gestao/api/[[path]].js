1d690623b7e00d0183e3335184523c88d0621287
JCAtayde <jc_atayde@hotmail.com>
2026-08-31 16:01:00 -0300

    Visualização

--- functions/gestao/api/[[path]].js
+++ functions/gestao/api/[[path]].js
@@ -59,7 +59,7 @@ export async function onRequest(context) {
         pecas:    (pec.results || []),
         fornecedores: (forn.results || []),
         logs: (lgs.results || []),
-        app_versao: '2026-08-30.1',
+        app_versao: '2026-08-31.1',
         config,
       });
     }
