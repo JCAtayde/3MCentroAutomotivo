594ad0ce4bb5d741facd95aa3a16408dee1eba63
JCAtayde <jc_atayde@hotmail.com>
2026-09-15 11:30:37 -0300

    Rejeitar

--- functions/gestao/api/[[path]].js
+++ functions/gestao/api/[[path]].js
@@ -62,7 +62,7 @@ export async function onRequest(context) {
         pecas:    (pec.results || []),
         fornecedores: (forn.results || []),
         logs: (lgs.results || []),
-        app_versao: '2026-08-31.3',
+        app_versao: '2026-09-15.1',
         config,
       });
     }
