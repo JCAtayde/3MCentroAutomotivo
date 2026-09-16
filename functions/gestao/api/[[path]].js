db63453e57e4395013e46be89369281d17b2aac4
JCAtayde <jc_atayde@hotmail.com>
2026-09-16 15:52:55 -0300

    permissoes

--- functions/gestao/api/[[path]].js
+++ functions/gestao/api/[[path]].js
@@ -63,7 +63,7 @@ export async function onRequest(context) {
         pecas:    (pec.results || []),
         fornecedores: (forn.results || []),
         logs: (lgs.results || []),
-        app_versao: '2026-09-15.8',
+        app_versao: '2026-09-15.9',
         config,
       });
     }
