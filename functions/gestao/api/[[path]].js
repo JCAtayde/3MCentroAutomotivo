d3da2b64f0515d992359730482e2e0b08ce903bb
JCAtayde <jc_atayde@hotmail.com>
2026-09-16 15:30:13 -0300

    RELATORIO

--- functions/gestao/api/[[path]].js
+++ functions/gestao/api/[[path]].js
@@ -63,7 +63,7 @@ export async function onRequest(context) {
         pecas:    (pec.results || []),
         fornecedores: (forn.results || []),
         logs: (lgs.results || []),
-        app_versao: '2026-09-15.6',
+        app_versao: '2026-09-15.7',
         config,
       });
     }
