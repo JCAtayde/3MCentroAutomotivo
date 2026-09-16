31ff2e1b84ed583636412a7a2486cb6ed28275b0
JCAtayde <jc_atayde@hotmail.com>
2026-09-16 11:39:22 -0300

    Data de Fechamento

--- functions/gestao/api/[[path]].js
+++ functions/gestao/api/[[path]].js
@@ -63,7 +63,7 @@ export async function onRequest(context) {
         pecas:    (pec.results || []),
         fornecedores: (forn.results || []),
         logs: (lgs.results || []),
-        app_versao: '2026-09-15.4',
+        app_versao: '2026-09-15.5',
         config,
       });
     }
